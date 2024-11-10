54f4563ac8cbd59ba1115bb1538109b3acb6f2e8
dockerwin11 <billywongkk@yahoo.com.hk>
2024-11-10 23:04:54 +0800

    updated 20241110

--- js/slide.js
+++ js/slide.js
@@ -1,12 +1,3 @@
-// Humburger button link
-const navBtn = document.getElementById("nav-mobile-toggle");
-const navLinks = document.getElementById("mobile-nav-list");
-
-
-navBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
-
 // Slide button
 let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
@@ -23,7 +14,3 @@ prev.addEventListener('click', function(){
     document.querySelector('.slide').prepend(items[items.length - 1]) 
     
 })
-
-// Add date for footer
-const date = document.getElementById("date");
-date.innerHTML = new Date().getFullYear();
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 Personal class project
 
 https://dockerwin11.github.io/studypage-personal/
+
+
+20241110 ver 1
+updated index.html, index.css, index.js AboutUs.jpg
+added   watercanvas.js slide.js AboutUs.html 
